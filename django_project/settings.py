9463d09f3d3de1a54b86b6ab19d8df8a9d181230
venomkiki <clintonfungai@gmail.com>
2019-06-07 10:41:04 +0300

    added db

--- django_project/settings.py
+++ django_project/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
 DATABASES = {}
-DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES['default'] = dj_database_url.config(default='postgres://localhost/clintondb',conn_max_age=600)
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.pyscopg2',
