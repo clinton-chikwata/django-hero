2d3de41f34e6de9844a4e913961a6363b98cc32c
venomkiki <clintonfungai@gmail.com>
2019-06-10 10:47:42 +0300

    add readme file

---
+++ blog/__init__.py
@@ -0,0 +1,12 @@
+[![Build Status](https://travis-ci.org/venomkiki/django-hero.svg?branch=master)](https://travis-ci.org/venomkiki/django-hero)
+
+#A simple web based application built using django framework.
+
+#Technologies used include :
+
+Postgres
+Django-framework
+Travis CI
+Heroku Server
+
+The application was  built using a test driven development approach and the tests were done using Travis CI and was deployed on heroku
