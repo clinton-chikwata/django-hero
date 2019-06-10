f035839a14bef87d519dc16053e0539de63cdba0
venomkiki <clintonfungai@gmail.com>
2019-06-10 10:30:38 +0300

    sqllite conf

--- django_project/settings.py
+++ django_project/settings.py
@@ -110,12 +110,12 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
